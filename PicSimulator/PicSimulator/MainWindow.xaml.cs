d54d41a4b33de2c9592c626bf5c7fd7c19037e34
tanja2003 <127766521+tanja2003@users.noreply.github.com>
2024-04-09 19:20:33 +0200

    Add files via upload
    Datei und Load Button
    Datei reinladen uber die MenuItem_Click Funktion
    ProgrammFile Klasse erstellt

---
+++ PicSimulator/PicSimulator/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PicSimulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Menu
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();  // to select a file for the user
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); // Standardverzeichnis
+            openFileDialog.Filter = "LST files (*.LST)|*.LST|Text files (*.txt)|*.txt|All Files (*.*)|*.*";  // onlz lst and txt files
+            openFileDialog.Multiselect = false;   // to select only one file
+
+            if(openFileDialog.ShowDialog() == true)
+            {
+                string selectedFileName = openFileDialog.FileName;
+
+            }
+        }
+    }
+    #endregion
+
+
+}
---
+++ PicSimulator/PicSimulator/NewFolder/Storage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicSimulator.NewFolder
+{
+    public class Storage
+    {
+
+        public Storage()
+        {
+            programmMemory = new Int32[1024];
+            stack = new int[8];
+
+        }
+
+        public Int32[] programmMemory;
+        public int programmCounter = 0;
+        public int[] stack;
+    }
+}
